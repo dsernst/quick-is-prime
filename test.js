e2627d48759348fc945a92bc82660482ba752193
David Ernst <git@dsernst.com>
2015-09-03 18:20:23 -0700

    Add more tests for timing of large numbers, special cases, and strings

--- test.js
+++ test.js
@@ -14,14 +14,28 @@ describe('isPrime', function () {
     assert(isPrime(100) === false);
   });
 
-  it('works for larger numbers too', function () {
+  it('can test a 7-digit numbers in under two seconds', function () {
+    var start = process.hrtime();
     assert(isPrime(9998903) === true);
+    var durationInNanoseconds = process.hrtime(start)[1];
+    assert(durationInNanoseconds < 2000000000);
   });
 
-  it('takes less than a millisecond to test numbers less than the highest already tested', function () {
+  it('takes under a millisecond for numbers less than the highest already tested', function () {
     var start = process.hrtime();
     assert(isPrime(9893899) === true);
     var durationInNanoseconds = process.hrtime(start)[1];
     assert(durationInNanoseconds < 1000000);
   });
+
+  it('returns false for 0, 1, and negative numbers', function () {
+    assert(isPrime(0) === false);
+    assert(isPrime(1) === false);
+    assert(isPrime(-3) === false);
+  });
+
+  it('returns false for strings', function () {
+    assert(isPrime('hello') === false);
+  });
+
 });
